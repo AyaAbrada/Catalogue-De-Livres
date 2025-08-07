0c9b7a00418bfaea4b32b32cc27f641bf92c7d08
AyaAbrada <abradaaya2@gmail.com>
2025-08-07 15:15:04 +0100

    Affichage des book Lister les livres publiés entre deux dates

--- src/main/java/com/example/cataloguedelivres/Repository/BookRepository.java
+++ src/main/java/com/example/cataloguedelivres/Repository/BookRepository.java
@@ -1,8 +1,12 @@
 package com.example.cataloguedelivres.Repository;
 import com.example.cataloguedelivres.Entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository< Book, Long> {
+@Query(value = "select * from book where date between :start and :and" , nativeQuery = true)
+    List<Book> findBooksBetweenDates();
 }

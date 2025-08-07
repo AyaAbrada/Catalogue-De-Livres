601e231fa30c370f5451d48410ac49c94124d0b5
AyaAbrada <abradaaya2@gmail.com>
2025-08-07 11:55:22 +0100

    first commit

---
+++ src/main/java/com/example/cataloguedelivres/CatalogueDeLivresApplication.java
@@ -0,0 +1,13 @@
+package com.example.cataloguedelivres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueDeLivresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueDeLivresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cataloguedelivres/CatalogueDeLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cataloguedelivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueDeLivresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

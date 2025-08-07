78a572fddcea4abf2ad6c074f8d1e57fa68db7d7
AyaAbrada <abradaaya2@gmail.com>
2025-08-07 15:36:45 +0100

    3Afficher les auteurs dont le prénom commence par la lettre A

--- src/main/java/com/example/cataloguedelivres/Repository/AuthorRepository.java
+++ src/main/java/com/example/cataloguedelivres/Repository/AuthorRepository.java
@@ -1,8 +1,13 @@
 package com.example.cataloguedelivres.Repository;
 import com.example.cataloguedelivres.Entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Query(value = "select * from author where name like A%" , nativeQuery = true)
+    List<Author> CommenceParLettre ();
 }

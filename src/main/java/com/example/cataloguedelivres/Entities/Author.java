d11ed6ce3f3826b24362bce25c00ee50901a8059
AyaAbrada <abradaaya2@gmail.com>
2025-08-07 12:11:14 +0100

    Création des entity

---
+++ src/main/java/com/example/cataloguedelivres/Entities/Author.java
@@ -0,0 +1,58 @@
+package com.example.cataloguedelivres.Entities;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String biography;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+
+    public Author(){}
+
+    public Author(Long id, String name, String biography , List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/example/cataloguedelivres/Entities/Book.java
@@ -0,0 +1,58 @@
+package com.example.cataloguedelivres.Entities;
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author-id")
+    private Author author;
+
+
+    public Book(){}
+
+    public Book(Long id, String title, String isbn, Author author) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
